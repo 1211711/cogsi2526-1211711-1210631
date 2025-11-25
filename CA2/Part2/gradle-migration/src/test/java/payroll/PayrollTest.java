ec348aa4bf6c2234571e670f8d4303ca47abc0cd
Joao Sousa (1210631) <1210631@isep.ipp.pt>
2025-11-25 22:44:14 +0000

    Add tests #28

---
+++ CA2/Part2/gradle-migration/src/test/java/payroll/PayrollTest.java
@@ -0,0 +1,14 @@
+package payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PayrollTest {
+
+    @Test
+    public void testPayroll() {
+        assertTrue(true, "The application context should load without errors");
+    }
+}
